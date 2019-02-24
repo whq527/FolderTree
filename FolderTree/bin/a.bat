e34318090e85b67a91cd0b186a1d0b6ef0e8bd08
whq527 <whq527@gmail.com>
2019-02-24 22:25:44 +0800

    Initial commit

---
+++ FolderTree/bin/a.bat
@@ -0,0 +1,10 @@
+rem eg:                                 FolderTree.exe "F:\folder"
+rem eg change separator:                FolderTree.exe -s "F:\folder"
+rem eg show write time:                 FolderTree.exe -w "F:\folder"
+rem eg show md5:                        FolderTree.exe -m "F:\folder"
+rem eg only show folder have files:     FolderTree.exe -o "F:\folder"
+rem eg filter file extension:           FolderTree.exe -f "F:\folder" ".exe;.zip"
+rem eg exclude file extension:          FolderTree.exe -e "F:\folder" ".exe"
+rem eg:                                 FolderTree.exe -wf "F:\folder" ".exe;.zip"
+
+start FolderTree.exe -wms "E:\OneDrive\¿ª·¢"
